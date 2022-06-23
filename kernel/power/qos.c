70f69d0388c63e78d22160bef8a4979467c12e30
RayapKun <rayapkun@gmail.com>
2022-06-23 02:09:34 +0000

    Merge commit of `android-4.19-stable` [1/2]
    * https://android.googlesource.com/kernel/common/+/refs/heads/android-4.19-stable
    
    Signed-off-by: RayapKun <rayapkun@gmail.com>
    
      Conflicts:
    	arch/Kconfig
    	arch/arm/Kconfig
    	arch/arm64/Kconfig
    	arch/arm64/include/asm/cpucaps.h
    	arch/arm64/kernel/vdso/Makefile
    	build.config.aarch64
    	build.config.x86_64
    	drivers/android/binder.c
    	drivers/base/core.c
    	drivers/clk/clk.c
    	drivers/dma-buf/dma-buf.c
    	drivers/scsi/sd.c
    	drivers/staging/android/ion/ion.c
    	drivers/usb/gadget/function/f_accessory.c
    	fs/ext4/namei.c
    	fs/incfs/main.c
    	include/asm-generic/vmlinux.lds.h
    	include/linux/android_kabi.h
    	include/linux/pci.h
    	include/linux/sched.h
    	include/linux/usb/usbnet.h
    	include/linux/virtio_net.h
    	kernel/power/qos.c
    	mm/filemap.c
    	net/wireless/util.c
    	sound/soc/soc-core.c

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -318,7 +318,7 @@ static inline int pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
  *  otherwise.
  */
 int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
-			 enum pm_qos_req_action action, int value)
+			 enum pm_qos_req_action action, int value, bool dev_req)
 {
 	int prev_value, curr_value, new_value;
 	struct cpumask cpus;
@@ -508,7 +508,7 @@ static void __pm_qos_update_request(struct pm_qos_request *req,
 	if (new_value != req->node.prio)
 		pm_qos_update_target(
 			pm_qos_array[req->pm_qos_class]->constraints,
-			&req->node, PM_QOS_UPDATE_REQ, new_value);
+			&req->node, PM_QOS_UPDATE_REQ, new_value, false);
 }
 
 /**
@@ -542,7 +542,7 @@ static void pm_qos_irq_release(struct kref *ref)
 	spin_unlock_irqrestore(&pm_qos_lock, flags);
 
 	pm_qos_update_target(c, &req->node, PM_QOS_UPDATE_REQ,
-			c->default_value);
+			c->default_value, false);
 }
 
 static void pm_qos_irq_notify(struct irq_affinity_notify *notify,
@@ -566,9 +566,8 @@ static void pm_qos_irq_notify(struct irq_affinity_notify *notify,
 
 	spin_unlock_irqrestore(&pm_qos_lock, flags);
 
-	if (affinity_changed)
-		pm_qos_update_target(c, &req->node, PM_QOS_UPDATE_REQ,
-				     req->node.prio);
+	pm_qos_update_target(c, &req->node, PM_QOS_UPDATE_REQ, req->node.prio,
+			false);
 }
 #endif
 
@@ -648,7 +647,7 @@ void pm_qos_add_request(struct pm_qos_request *req,
 	INIT_DELAYED_WORK(&req->work, pm_qos_work_fn);
 	trace_pm_qos_add_request(pm_qos_class, value);
 	pm_qos_update_target(pm_qos_array[pm_qos_class]->constraints,
-			     &req->node, PM_QOS_ADD_REQ, value);
+			     &req->node, PM_QOS_ADD_REQ, value, false);
 
 #ifdef CONFIG_SMP
 	if (req->type == PM_QOS_REQ_AFFINE_IRQ &&
@@ -663,7 +662,7 @@ void pm_qos_add_request(struct pm_qos_request *req,
 			cpumask_setall(&req->cpus_affine);
 			pm_qos_update_target(
 				pm_qos_array[pm_qos_class]->constraints,
-				&req->node, PM_QOS_UPDATE_REQ, value);
+				&req->node, PM_QOS_UPDATE_REQ, value, false);
 		}
 	}
 #endif
@@ -720,7 +719,7 @@ void pm_qos_update_request_timeout(struct pm_qos_request *req, s32 new_value,
 	if (new_value != req->node.prio)
 		pm_qos_update_target(
 			pm_qos_array[req->pm_qos_class]->constraints,
-			&req->node, PM_QOS_UPDATE_REQ, new_value);
+			&req->node, PM_QOS_UPDATE_REQ, new_value, false);
 
 	schedule_delayed_work(&req->work, usecs_to_jiffies(timeout_us));
 }
@@ -760,7 +759,7 @@ void pm_qos_remove_request(struct pm_qos_request *req)
 	trace_pm_qos_remove_request(req->pm_qos_class, PM_QOS_DEFAULT_VALUE);
 	pm_qos_update_target(pm_qos_array[req->pm_qos_class]->constraints,
 			     &req->node, PM_QOS_REMOVE_REQ,
-			     PM_QOS_DEFAULT_VALUE);
+			     PM_QOS_DEFAULT_VALUE, false);
 	memset(req, 0, sizeof(*req));
 }
 EXPORT_SYMBOL_GPL(pm_qos_remove_request);
