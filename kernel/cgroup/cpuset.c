83a58ab7f7df316d9c5f06797b06c708c2b7ae3a
minaripenguin37 <alexfinhart@gmail.com>
2022-10-11 06:14:34 +0800

    uclamp_assist: Tune based on device tree libperfmgr changes
    * based on raven

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -2090,13 +2090,13 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 0, 1},
-		{"foreground", 	     	"0",  "50",  1, 1},
-		{"background", 	     	"20", "100", 0, 0},
-		{"system-background", 	"0",  "30",  0, 0},
-		{"camera-daemon",	"50", "100", 1, 1},
+		{"top-app",             "103", "1024", 1, 1},
+		{"foreground",          "0", "1024",  1, 0},
+		{"background",          "0",  "50",  0, 0},
+		{"system-background",   "0",  "50",  0, 0},
+		{"camera-daemon",       "1", "1024", 0, 1},
 		{"display",             "50", "100", 1, 1},
-		{"restricted", 		"0", "30", 0, 0},
+		{"restricted",          "0",  "50",  0, 0},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
